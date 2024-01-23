0cdb4f2ae833eb501e8fa5c5851efd5d041633cc
leily <leily@example.com>
2024-01-23 10:37:24 +0300

    test failed

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    @Test
+    void shouldTestForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванова Татьяна");
+        $("[data-test-id=phone] input").setValue("+79001234567");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Твоя заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
